7a7b0e3972f81bbd7554bf437fdf4bf522150323
brian <fil419@webmail.co.za>
2022-02-06 17:36:29 +0200

    p[roject files

---
+++ script.js
@@ -0,0 +1,54 @@
+const timerContainer = document.querySelector(".timer-container");
+const items = document.querySelectorAll(".timer-format h2");
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getHours();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 0, 0, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const day = futureDate.getDate();
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+	const today = new Date().getTime();
+	const t = futureTime - today;
+
+	const oneDay = 24 * 60 * 60 * 1000;
+	const oneHour = 60 * 60 * 1000;
+	const oneMinute = 60 * 1000;
+
+	let days = Math.floor(t/oneDay);
+
+
+	let hours = Math.floor((t % oneDay) / oneHour);
+
+	let minutes = Math.floor((t % oneHour) / oneMinute);
+
+	let seconds = Math.floor((t % oneMinute) / 1000);
+
+	const values = [days, hours, minutes, seconds];
+
+	function format(item) {
+		if(item < 10) {
+			return item = `0${item}`;
+		}
+		else {
+			return item;
+		}
+	}
+
+	items.forEach(function(item, index){
+		item.innerHTML = format(values[index]);
+	});
+	if(t < 0) {
+		clearInterval(countdown);
+		timerContainer.innerHTML = `<h2 class="expired">sorry, this launch has started</h2>`
+	}
+};
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
